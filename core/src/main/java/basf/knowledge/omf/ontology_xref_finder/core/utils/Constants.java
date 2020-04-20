40e02710ed57b78c24d6a79d0c91bf0fb4db4676
José Antonio <neoxisti@gmail.com>
2020-04-20 17:42:51 +0200

    Adding xref definition to CSV output

--- core/src/main/java/basf/knowledge/omf/ontology_xref_finder/core/utils/Constants.java
+++ core/src/main/java/basf/knowledge/omf/ontology_xref_finder/core/utils/Constants.java
@@ -18,5 +18,5 @@ public enum Constants {
 	public static String HAS_NARROW_SYN = "hasNarrowSynonym";
 	public static String HAS_RELATED_SYN = "hasRelatedSynonym";
 	public static final boolean DEV_MODE = true;
-	public static final boolean DEBUG_MODE = true;
+	public static final boolean DEBUG_MODE = false;
 }
