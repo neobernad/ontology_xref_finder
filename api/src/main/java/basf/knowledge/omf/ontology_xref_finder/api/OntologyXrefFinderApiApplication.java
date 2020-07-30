14c83b41b004d80777867c2486288b33c5de7006
José Antonio <neoxisti@gmail.com>
2020-07-30 10:04:19 +0200

    Adding fix when using a reverse proxy

--- api/src/main/java/basf/knowledge/omf/ontology_xref_finder/api/OntologyXrefFinderApiApplication.java
+++ api/src/main/java/basf/knowledge/omf/ontology_xref_finder/api/OntologyXrefFinderApiApplication.java
@@ -2,8 +2,12 @@ package basf.knowledge.omf.ontology_xref_finder.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@Configuration
+@PropertySource("classpath:application.properties")
 public class OntologyXrefFinderApiApplication {
 
 	public static void main(String[] args) {
